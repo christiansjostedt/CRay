e3f1b991cadd8c34893a335e95a6ca6570eb4662
Christian Sjostedt <c.sjostedt@hotmail.com>
2012-12-06 21:11:20 +1100

    Added some files. + wrote a new Type, Ray.h

---
+++ Debug/src/Cameras/Camera.d
@@ -0,0 +1,6 @@
+src/Cameras/Camera.d src/Cameras/Camera.o: ../src/Cameras/Camera.cpp \
+  ../src/Cameras/Camera.h ../src/Cameras/../Libraries/vec3.h
+
+../src/Cameras/Camera.h:
+
+../src/Cameras/../Libraries/vec3.h:
--- src/CRay.cpp
+++ src/CRay.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <time.h>
 #include "Libraries/vec3.h"
+#include "Libraries/Ray.h"
 #include "Cameras/Camera.h"
 #include "Magick++.h"
 #include <string>
@@ -16,18 +17,61 @@
 using namespace std;
 using namespace Magick;
 
+void UVColorTest(Image &image, int x,int y)
+{
+	double newX = (x+0.0001)/50*MaxRGB;
+	double newY = (y+0.0001)/50*MaxRGB;
+	image.pixelColor(x,y,Color(newX,newY,0,1));
+	//cout<<"x:"<<x<<"  y: "<<y<<"  Color:("<<newX/MaxRGB<<","<<newY/MaxRGB<<","<<0/MaxRGB<<","<<1/MaxRGB<<")"<<endl;
+}
+
+void rayThroughPixel(Camera &cam, int x, int y)
+{
+	/*Create a Coordinate frame
+	vec3 w;
+	vec3 a;
+	vec3 b;
+	vec3 u;
+	vec3 v;
+
+	a = eyePosition - centerPos;
+	b = upVector
+
+	w = a.normailize()
+	u = (b X w).normalize()
+	v = w X u
+
+
+	Image Canvas will be at 1*-w or -w. 1 unit away from the camera in world-space coordniates
+
+	alphaU = tan(fov*X / 2) * (( xPixel-(imageWidth/2)) / (imageWidth/2) )
+	alphaU = horizontal pixels
+
+	betaV = tan(fov*Y / 2) * ( ((imageHeight/2)-yPixel) / (imageHeight/2))
+	betaV = Vertical pixels
+
+	//Equation for a ray
+	ray = ( eye + (alphaU + betaV-W).normalize() )
+	ray = (origin + normalized direction)
+
+	Calculating intersection is:
+	ray = (origin + normalized direction)   * DistanceAlongRay
+	ray = ( eye + (alphaU + betaV-W).normalize() ) * DistanceAlongRay
+	*/
+}
 
 int main(int argc,char **argv)
 {
   //Initialization Vars:
-	int ImgResX = 50;
+	int ImgResX = 80;
 	int ImgResY = 50;
 
-	//Canvas size string, concactenate int and strings
+	//Canvas size string, Concactenating int and strings
 	ostringstream oss;
 	oss << ImgResX << "x"<< ImgResY;
 	string imgSize = oss.str();
 
+	//Write to this destination
 	string filename = "/Users/christiansjostedt/Desktop/CRay_render.png";
 
 	//Camera Cam;
@@ -50,11 +94,7 @@ int main(int argc,char **argv)
 			//Intersection hit=Intersect(ray,scene);
 			//Image[x,y] = FindColor(hit);
 
-			double newX = (x+0.0001)/50*MaxRGB;
-			double newY = (y+0.0001)/50*MaxRGB;
-			image.pixelColor(x,y,Color(newX,newY,0,1));
-			//cout<<"x:"<<x<<"  y: "<<y<<"  Color:("<<newX/MaxRGB<<","<<newY/MaxRGB<<","<<0/MaxRGB<<","<<1/MaxRGB<<")"<<endl;
-			//image.pixelColor(50,50,Color("red")); // set the pixel at position (50,50) to red
+			UVColorTest(image, x, y);
 		}
 	}
 
@@ -66,6 +106,10 @@ int main(int argc,char **argv)
 
 	//Write to disk
 	image.write( filename );
+
+	Ray ray = Ray(0,0,0);
+	Ray gay = Ray(1,3,4);
+	cout << gay+ray;
 
 
 	return 0;
